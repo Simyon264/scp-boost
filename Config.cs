7a6b548719edddac49cac9b95b105257c43c6eec
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2022-04-17 01:07:38 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace scp_boost
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public Dictionary<RoleType, byte> Boost { get; set; } = new Dictionary<RoleType, byte>()
+        {
+            { RoleType.Scp049, 5}
+        };
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,61 @@
+﻿using Exiled.API.Enums;
+using Features = Exiled.API.Features;
+using ServerEvents = Exiled.Events.Handlers.Server;
+using PlayerEvents = Exiled.Events.Handlers.Player;
+using Exiled.Events.EventArgs;
+using MEC;
+
+namespace scp_boost
+{
+    public class Plugin : Features.Plugin<Config>
+    {
+        public override string Name { get; } = "SCP-Boost";
+        public override string Prefix { get; } = "scpboost";
+        public override string Author { get; } = "Simyon";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override PluginPriority Priority { get; } = PluginPriority.High;
+
+        private static readonly Plugin InstanceValue = new Plugin();
+        private Plugin()
+        {
+
+        }
+
+        public static Plugin StaticInstance => InstanceValue;
+
+        public override void OnEnabled()
+        {
+            if (!Config.IsEnabled) return;
+
+            PlayerEvents.ChangingRole += Spawn;
+            PlayerEvents.Died += Died;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            PlayerEvents.ChangingRole -= Spawn;
+            PlayerEvents.Died -= Died;
+            base.OnDisabled();
+        }
+
+        public void Spawn(ChangingRoleEventArgs ev)
+        {
+            Timing.CallDelayed(1f, () =>
+             {
+                 foreach (KeyValuePair<RoleType, byte> entry in Config.Boost)
+                 {
+                     if (entry.Key == ev.NewRole)
+                     {
+                         ev.Player.EnableEffect("MovementBoost");
+                         ev.Player.ChangeEffectIntensity("MovementBoost", entry.Value);
+                     }
+                 }
+             });
+        }
+
+        public void Died(DiedEventArgs ev)
+        {
+            ev.Target.DisableAllEffects();
+        }
+    }
+}
